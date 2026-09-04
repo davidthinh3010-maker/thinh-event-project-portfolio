cb5ee2f615399fd9462007d73351c08ee73bf1a6
davidthinh3010-maker <davidthinh3010@gmail.com>
2026-09-04 17:11:07 +0700

    Enable optimized project image atlas

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import PortfolioApp from './PortfolioApp';
 import './portfolio.css';
+import './sprite.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
